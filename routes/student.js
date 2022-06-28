ab3168c9e29cd40c3f11ea732162ac5ed2fde3be
AmalDer <amalderouich00@gmail.com>
2022-06-28 21:26:06 +0100

    first commit

---
+++ routes/student.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+var router = express.Router();
+
+var database = require('../database');
+
+router.get("/", function(request, response, next){
+
+	var query = "SELECT * FROM student ORDER BY id DESC";
+
+	database.query(query, function(error, data){
+
+		if(error)
+		{
+			throw error; 
+		}
+		else
+		{
+			response.render('student', {title:'Node.js MySQL CRUD Application', action:'list', sampleData:data});
+		}
+
+	});
+
+});
+
+module.exports = router;
